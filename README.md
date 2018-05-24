8f87fce8fd5b8693de3c990f5a1e109ee4aeb6be
Yann CATALA <yann@e55c.com>
2018-05-24 11:58:21 +0200

    Ajout des commentaire

---
+++ README.md
@@ -0,0 +1,4 @@
+To lunch the Maze :
+
+1.Open a termninal and go to the folder.
+2.type : make run
--- headers/fonctions.h
+++ headers/fonctions.h
@@ -35,53 +35,53 @@ extern char pseudo[4];
 
 					/**************************************************************Fonction du fichier labyrinthe********************************************************************/
 
-void initLab();
-void depLab();
-void moveCursor();
-void trace_cursor();
-void mur();
-void chemins();
-void entree();
-void sortie();
-void droite();
-void gauche();
-void bas();
-void haut();
-void fin();
-void affich_fin();
+void initLab();			// affiche le labyrinthe
+void depLab();			// permet de se déplacer dans le labyrinthe
+void moveCursor();		// fonction permettant de déplacer le personnage dans le labyrinthe
+void trace_cursor();	// affiche le chemin du personnage
+void mur();				// affiche les murs
+void chemins();			// affiche les chemins
+void entree();			// place l'entrée du labyrinthe
+void sortie();			// place la sortie du labyrinthe
+void droite();			// déplace le personnage vers la droite
+void gauche();			// déplace le personnage vers la gauche
+void bas();				// déplace le personnage vers le bas
+void haut();			// déplace le personnage vers le haut
+void fin();				// gère la sortie du labyrinthe
+void affich_fin();		// affiche le message de fin
 
 					/**************************************************************Fonction du fichier fonction********************************************************************/
 
 
-void initcurses();
-void initColors();
-void choix_lvl();
-void tab_score();
-void affich_score();
-void save();
-void reprendre();
-void choix_pseudo();
-void bienvenue();
-void prog_princ();
+void initcurses();		// initialise les fonctions nécessaires à l'utilisation de ncurses
+void initColors();		// initialise les fonctions d'affichage des couleurs
+void choix_lvl();		// permet de choisir le chemin vers le fichier de la map du labyrinthe
+void tab_score();		// enregistre les scores dans un fichier .txt
+void affich_score();	// affiche le tableau des scores
+void save();			// sauvegarde la position du personnage dans un fichier .txt
+void reprendre();		// lis les informations dans le fichier de sauvegarde
+void choix_pseudo();	// permet de choisir son pseudo
+void bienvenue();		// affiche la page de préseantation du jeu au lancement
+void prog_princ();		// programme réunissant toute les fonctions nécessaire au labyrinthe
 
 					/**************************************************************Fonction du fichier menu.c********************************************************************/
 
-void menu_princ();
-void menu_lvl();
-void menu_game();
+void menu_princ();		// fonction servant à l'affichage du menu principal
+void menu_lvl();		// fonction servant à l'affichage du choix du niveau	
+void menu_game();		// fonction servant à l'affichage du menu pause
 
-void choix_menu();
-void choix_menu_lvl();
-void choix_menu_game();
+void choix_menu();		// affiche les différente possibilité du menu principal 
+void choix_menu_lvl();	// affiche les différente possibilité du menu du choix du niveau
+void choix_menu_game();	// affiche les différente possibilité du menu pause
 
-void dep_menu();
-void dep_menu_lvl();
-void dep_menu_game();
+void dep_menu();		// permet de se déplacer dans le menu principal
+void dep_menu_lvl();	// permet de se déplacer dans le menu du choix du niveau
+void dep_menu_game();	// permet de se déplacer dans le menu pause
 
-void titre_menu();
-void suppr_menu();
-void creation_items();
-void print_in_middle();
+void titre_menu();		// permet d'afficher le titre du menu	
+void suppr_menu();		// permet de supprimer le menu afficher
+void creation_items();	// permet de créer les differents choix possible
+void print_in_middle();	// permet de centrer l'affichage du titre du menu
 
 
 #endif
